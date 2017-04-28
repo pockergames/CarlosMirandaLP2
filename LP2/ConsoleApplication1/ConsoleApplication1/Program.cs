484c6b25cf20b8f5e3d1ddca5591b33b14cb525c
PockerGames <pockergames@gmail.com>
2017-04-28 18:53:55 -0300

    First step done (it was actually remade)

--- LP2/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ LP2/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -12,20 +12,49 @@ namespace ConsoleApplication1
         {
             int carros, i = 0;
             Console.WriteLine("Informe o a quantidade de carros a serem analisados");
-            carro = Console.ReadLine();
-            int[] modelo = new int[carro-1];
-            double[] km = new double[carro - 1];
-            double[] pot = new double[carro - 1];
-            while(i < carros - 1)
+            carros = Convert.ToInt32(Console.ReadLine());
+            string[] modelo = new string[carros];
+            double[] km = new double[carros];
+            double[] pot = new double[carros];
+            string[] classKm = new string[carros];
+            string[] classPot = new string[carros];
+            while (i < carros)
             {
-                Console.WriteLine("Informe o modelo do carro {0}", i);
+                Console.WriteLine("Informe o modelo do carro {0}", i + 1);
                 modelo[i] = Console.ReadLine();
-                Console.WriteLine("Informe a quilometragem do carro {0}", i);
-                km[i] = Console.ReadLine();
-                Console.WriteLine("Informe a potência do carro {0}", i);
-                pot[i] = Console.ReadLine();
-
+                Console.WriteLine("Informe a quilometragem do carro {0}", i + 1);
+                km[i] = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Informe a potência do carro {0}", i + 1);
+                pot[i] = Convert.ToDouble(Console.ReadLine());
+            
+                if (km[i] <= 5000)
+                {
+                    classKm[i] = "Novo";
+                }
+                else if (km[i] > 5000 && km[i] <= 30000)
+                {
+                    classKm[i] = "Semi-novo";
+                }
+                else if (km[i] > 30000)
+                {
+                    classKm[i] = "Velho";
+                }
+                if (pot[i] < 120)
+                {
+                    classPot[i] = "Popular";
+                }
+                else if (pot[i] >= 120 && pot[i] <= 200)
+                {
+                    classPot[i] = "Forte";
+                }
+                else if (pot[i] > 200)
+                {
+                    classPot[i] = "Potente";
+                }
+                Console.WriteLine("{0} - {1} - {2}", modelo[i], classKm[i], classPot[i]);
+                i++;
             }
+            
 
 
 

4ccf0aae3c685ab76546e548047bf5d1aa2b2d93
PockerGames <pockergames@gmail.com>
2017-05-17 16:05:50 -0300

    Programa feito

---
+++ MesPorExtensos/MesPorExtensos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MesPorExtensos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dia, mes, ano;
+            string[] meses =  {"janeiro", "fevereiro", "março", "abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro"};
+            string data;
+            Console.WriteLine("Informe a data");
+            data = Console.ReadLine();
+            dia = Convert.ToInt32(data.Substring(0, 2));
+            mes = Convert.ToInt32(data.Substring(3, 2));
+            ano = Convert.ToInt32(data.Substring(6, 4));
+            Console.WriteLine("{0} de {1} de {2}", dia, meses[mes-1], ano);
+            }
+
+            }
+        }
+    
+

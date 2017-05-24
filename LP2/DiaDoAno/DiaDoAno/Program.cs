21d52bb33a6322fd1ebb81d8f404c41b3cdd37b2
PockerGames <pockergames@gmail.com>
2017-05-23 21:21:49 -0300

    Funciona p/ dias acima de 9

---
+++ LP2/DiaDoAno/DiaDoAno/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiaDoAno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mes = new int[12];
+            for (int i = 1; i < mes.Length; i++)
+            {
+                if (i == 2)
+                    mes[i] = mes[i - 1] + 28;
+                else if (i % 2 == 0)
+                    mes[i] = mes[i - 1] + 30;
+                else
+                    mes[i] = mes[i - 1] + 31;
+            }
+            string []mesEx = { " ", "janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro" };
+            int dia;
+            string data;
+            Console.WriteLine("Informe a data");
+            data = Console.ReadLine();
+            dia = Convert.ToInt32(data.Substring(0, 2));
+            for(int i = 1; i < mes.Length; i++)
+            {
+                if (mesEx[i] == data.Substring(6, data.Length - 6))
+                {
+                    Console.WriteLine(mes[i - 1] + dia + "° dia do ano");
+                }
+            }
+
+           
+
+
+        }
+    }
+}

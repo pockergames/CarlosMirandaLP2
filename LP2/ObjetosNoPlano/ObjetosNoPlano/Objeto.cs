def419812aefe2c6c2bafab405687330cd991470
PockerGames <pockergames@gmail.com>
2017-06-06 14:09:22 -0300

    Primeira parte finalizada

---
+++ LP2/ObjetosNoPlano/ObjetosNoPlano/Objeto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjetosNoPlano
+{
+    class Objeto
+    {
+        public int x, y;
+
+        public void AndarDireita()
+        {
+            x += 3;
+        }
+        public void AndarEsquerda()
+        {
+            x -= 3;
+        }
+        public void AndarCima()
+        {
+            y += 3;
+        }
+        public void AndarBaixo()
+        {
+            y -= 3;
+        }
+        
+
+        public string Coordenadas()
+        {
+            string coord;
+            coord = ("(" + x + "," + y + ")");
+            return coord;
+        }
+
+
+    }
+}
---
+++ LP2/ObjetosNoPlano/ObjetosNoPlano/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjetosNoPlano
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int l = 600, a = 800;
+            int[,] plano = new int[l, a];
+            ConsoleKey comando;
+            Objeto obj = new Objeto();
+            Console.WriteLine("Posição inicial do Objeto");
+            obj.x = int.Parse(Console.ReadLine());
+            obj.y = int.Parse(Console.ReadLine());
+            
+
+            
+                while (true)
+                {
+                    comando = Console.ReadKey().Key;
+
+                    if (comando == ConsoleKey.RightArrow && obj.x <= l - 3 &&  obj.y <= a)
+                        {
+                            obj.AndarDireita();
+                            Console.WriteLine(obj.Coordenadas());
+                        }
+                    
+
+
+                }
+                
+            
+            
+
+
+
+
+
+        }
+    }
+}

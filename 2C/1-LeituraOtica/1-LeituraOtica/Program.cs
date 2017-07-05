87ac28431b1a5ebabcc109953cb1dcb54a4b92d9
PockerGames <pockergames@gmail.com>
2017-07-05 15:35:50 -0300

    Leitura-Otica 100%

---
+++ 2C/1-LeituraOtica/1-LeituraOtica/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_LeituraOtica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            Questao[] q = new Questao[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                q[i] = new Questao();
+                q[i].a = Convert.ToInt32(Console.ReadLine());
+                q[i].b = Convert.ToInt32(Console.ReadLine());
+                q[i].c = Convert.ToInt32(Console.ReadLine());
+                q[i].d = Convert.ToInt32(Console.ReadLine());
+                q[i].e = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(q[i].Certa());
+            }
+        }
+    }
+}

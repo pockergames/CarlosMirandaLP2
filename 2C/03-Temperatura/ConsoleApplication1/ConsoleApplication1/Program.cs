d156b1731d31b655fb013e07af5085680d758862
PockerGames <pockergames@gmail.com>
2017-08-02 14:51:43 -0300

    imcomp

---
+++ 2C/03-Temperatura/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===========================Temperature Converter BETA 0.1=======================");
+
+            CelciusTemperature x = new CelciusTemperature();
+
+            Console.WriteLine("Insert the temperature value (only numbers*)");
+            x.value = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Value in Farenheit");
+            Console.WriteLine(x.FarenheitConvertion() + "°F");
+
+
+
+
+        }
+    }
+}
---
+++ 2C/03-Temperatura/ConsoleApplication1/ConsoleApplication1/Temperatura.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class CelciusTemperature
+    {
+        public double value;
+        public double f;
+        public double FarenheitConvertion()
+        {
+            f = (value * 1.8) + 32;
+            return f;
+        }
+
+        
+    }
+}

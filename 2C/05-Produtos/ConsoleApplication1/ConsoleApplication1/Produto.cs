1188ad15a6176f609fd158b4a9b16523ee23050a
PockerGames <pockergames@gmail.com>
2017-08-16 15:38:03 -0300

    05 - produtos

---
+++ 2C/05-Produtos/ConsoleApplication1/ConsoleApplication1/Produto.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Produto
+    {
+
+        private int id;
+        private string nome;
+        private double preco;
+
+        
+        public Produto(int id, string nome, double preco)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.preco = preco;
+            Quantidade = 0;
+        }
+        public int Id { 
+            get{
+                return this.id;
+            }
+            
+        }
+
+        public string Nome {
+            get
+            {
+                return nome;
+            }
+        }
+
+        public double Preco
+        {
+            get
+            {
+                return preco;
+            }
+        }
+        public int Quantidade { get; set; }
+
+        public void Reposicao(int qtd)
+        {
+            Quantidade += qtd;
+        }
+
+        public void Retirada(int qtd)
+        {
+            if (Quantidade >= qtd)
+                Quantidade -= qtd;
+            else
+                throw new Exception();
+        }
+        public string Imprimir()
+        {
+            return String.Format("{0} - {1} - R${2} - {3}", id, nome, preco, Quantidade);
+        }
+    }
+}
---
+++ 2C/05-Produtos/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto(1, "Sabonete O Cara 100g", 10.00);
+            p.Reposicao(100);
+            p.Retirada(20);
+
+            Console.WriteLine(p.Imprimir());
+        }
+    }
+}

212f3a699cfb8c348cc739c1c0dc2cc77ff13409
PockerGames <pockergames@gmail.com>
2017-09-05 20:55:23 -0300

    Conversion to Kg: -from g (done) -from t (in process)

---
+++ 2C/07-ConversorMedidas/ConversorMedidas/ConversorMedidas/Conversor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorMedidas
+{
+    static class Conversor
+    {
+
+        private static string valor;
+
+        public static string Valor
+        {
+            set
+            {
+                valor = value;
+            }
+        }
+
+        public static string ToKg()
+        {
+            double numero;
+            if (valor[valor.Length - 1] == 'g')
+            {
+                numero = double.Parse(valor.Substring(0, valor.Length - 1));
+
+                return Convert.ToString(numero / 1000) + "Kg";
+            }
+            else if(valor[valor.Length - 1] == 'T' || valor[valor.Length - 1] == 't')
+            {
+                numero = double.Parse(valor.Substring(0, valor.Length - 1));
+                return Convert.ToString(numero * 1000) + "Kg";
+            }
+            else
+            {
+                return "KG";
+            }
+                
+        }
+
+
+
+
+
+    }
+}
---
+++ 2C/07-ConversorMedidas/ConversorMedidas/ConversorMedidas/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorMedidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o valor da medida a ser convertida");
+            Conversor.Valor = Console.ReadLine();
+            Console.WriteLine(Conversor.ToKg());
+            
+        }
+    }
+}
